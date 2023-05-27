b9d9db9c5181db433e7f149b076f7dae00a5167a
argunidem <argun.idem@hotmail.com>
2023-05-27 11:54:53 +0200

    array prototype last

---
+++ 05_array-prototype-last.js
@@ -0,0 +1,23 @@
+// Array Prototype Last
+
+// Write code that enhances all arrays such that you can call the array.last() method on any array and it will return the last element. If there are no elements in the array, it should return -1.
+
+// Example 1:
+
+// Input: nums = [1,2,3]
+// Output: 3
+// Explanation: Calling nums.last() should return the last element: 3.
+// Example 2:
+
+// Input: nums = []
+// Output: -1
+// Explanation: Because there are no elements, return -1.
+
+Array.prototype.last = function () {
+  // Check if the array is empty
+  if (this.length === 0) {
+    return -1; // Return -1 if there are no elements
+  } else {
+    return this[this.length - 1]; // Return the last element of the array
+  }
+};
